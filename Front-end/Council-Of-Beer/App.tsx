33cf1fd59b298ccb8707b81d5683db1ab0e1e79b
nellaSoudunsaari <nellasoudunsaari@gmail.com>
2026-02-10 17:16:46 +0200

    added main bottom nav screens and navigation WIP

--- Front-end/Council-Of-Beer/App.tsx
+++ Front-end/Council-Of-Beer/App.tsx
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Profile from './screens/Profile';
+import Home from './screens/Home';
+import NewPost from './screens/NewPost';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
+
+  const Tabs = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>KaliaKunta Määrää</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tabs.Navigator>
+          <Tabs.Screen name="Home" component={Home}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Ionicons name="beer-outline" size={24} color="black" />
+              )
+            }}
+          />
+          <Tabs.Screen name="New Post" component={NewPost}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="add-circle-outline" size={24} color="black"/>
+              )
+            }}  
+          />
+          <Tabs.Screen name="Profile" component={Profile}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Ionicons name="person-circle-outline" size={24} color="black" />
+              )
+            }}
+          />
+        </Tabs.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
---
+++ Front-end/Council-Of-Beer/screens/Profile.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Profile(){
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.topbar}>
+                <Text>Council of Beer</Text>
+            </View>
+            <View style={styles.maincontainer}>
+                <Text>Profile screen - main content</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topbar:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  maincontainer: {
+    alignItems: 'center',
+    backgroundColor: '#a3a3a3',
+    justifyContent: 'center',
+    flex: 3
+  },
+});
