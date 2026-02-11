aabf280748e3f56b168c844db4441636adf26062
nellaSoudunsaari <nellasoudunsaari@gmail.com>
2026-02-11 18:51:42 +0200

    starting layout of profile screen

--- Front-end/Council-Of-Beer/screens/Profile.tsx
+++ Front-end/Council-Of-Beer/screens/Profile.tsx
@@ -1,20 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function Profile(){
 
     return(
-        <View style={styles.container}>
+        <>
+          <View style={styles.profileinfo}>
+            <View style={styles.pfpiccont}>
+              <Image style={styles.pfpic} source={require('../assets/beerpf.png')}/>
+            </View>
+            <View style={styles.pftextcont}>
+              <Text style={styles.username}>@ilovebeer</Text>
+              <View style={styles.pfstatscont}>
+                <Text>Ratings</Text>
+                <Text>Followers</Text>
+                <Text>Following</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.profilecontent}>
             <Text>Profile screen - main content</Text>
-        </View>
+          </View>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#888',
-    alignItems: 'center',
+  profileinfo:{
+    flex: 2,
+    flexDirection: 'row',
     justifyContent: 'center',
+    paddingTop: 20
+  },
+  profilecontent: {
+    flex: 5,
+  },
+  pfpic: {
+    width: 72,
+    height: 72,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+  pfpiccont: {
+    alignItems: 'center',
+    position: 'absolute',
+    top: 25,
+    left: 25
+  },
+  pftextcont: {
+    flexDirection: 'column'
+  },
+  username: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  pfstatscont: {
+    flexDirection: 'row',
+    columnGap: 20
   },
 });
