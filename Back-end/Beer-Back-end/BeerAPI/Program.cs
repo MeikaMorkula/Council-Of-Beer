22da07e64c99d1c9de870c01cb7b5a3771e6070b
qb-le <quocbaole2103@gmail.com>
2026-02-24 10:31:28 +0100

    connection added to program

--- Back-end/Beer-Back-end/BeerAPI/Program.cs
+++ Back-end/Beer-Back-end/BeerAPI/Program.cs
@@ -1,4 +1,11 @@
+using Npgsql;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 
 // Add services to the container.
 
@@ -8,6 +15,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddScoped<IDbConnection>(_ =>
+{
+    var cs = builder.Configuration.GetConnectionString("Default");
+    return new NpgsqlConnection(cs);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
