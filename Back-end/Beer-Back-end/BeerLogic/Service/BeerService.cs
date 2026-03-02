42f93a018cd4fbfe0aae937f2f1c0e03b6998ad8
WojtekZb <554101@student.fontys.nl>
2026-03-02 15:02:47 +0100

    After merge first test will be ready

--- Back-end/Beer-Back-end/BeerLogic/Service/BeerService.cs
+++ Back-end/Beer-Back-end/BeerLogic/Service/BeerService.cs
@@ -1,18 +1,57 @@
 ﻿using BeerLogic.DTOs;
-using BeerLogic.Mapper;
+using BeerLogic.Entities;
+using BeerLogic.Interface;
 
 namespace BeerLogic.Service
 {
     public class BeerService
     {
+        private readonly IBeerRepo _repo;
+        private readonly Mapper.Mapper _map;
+
+        public BeerService(Mapper.Mapper map, IBeerRepo repo)
+        {
+            _repo = repo;
+            _map = map;
+        }
         public List<BeerDTO> GetAllBeer()
         {
-
+            List<BeerDTO> beerlist = _repo.GetAllBeer();
+            return beerlist;
         }
 
         public BeerDTO AddBeer(BeerDTO beer)
         {
-            DTOToEntity
+            Beer EBeer = _map.BeerDTOToEntity(beer);
+            if (EBeer == null)
+            {
+                throw new Exception("Beer cant be empty");
+            }
+            if (EBeer.Name == null)
+            {
+                throw new Exception("Beer must have a name.");
+            }
+            if (EBeer.Brewery == null)
+            {
+                throw new Exception("Beer must be brewed somewhere.");
+            }
+            if (EBeer.Country == null)
+            {
+                throw new Exception("The beer must have an origin");
+            }
+            if (EBeer.AlcPrecentage == null)
+            {
+                throw new Exception("Dont leave alcohole percantage empty");
+            }
+            if (EBeer.Label == null)
+            {
+                throw new Exception("The beer must have a label.");
+            }
+            else
+            {
+                beer = _map.BeerEntityToDTO(EBeer);
+                return beer;
+            }
         }
     }
 }
