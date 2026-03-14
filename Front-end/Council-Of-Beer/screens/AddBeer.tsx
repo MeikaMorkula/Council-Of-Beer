1d3694418ff206920e30480d629194f47b083e5c
MeikaMorkula <187277094+MeikaMorkula@users.noreply.github.com>
2026-03-14 15:51:12 +0200

    Absolute spaghetti code

---
+++ Front-end/Council-Of-Beer/screens/AddBeer.tsx
@@ -0,0 +1,321 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { ScrollView } from "react-native";
+import { useTranslation } from "react-i18next";
+
+export default function AddBeer() {
+  const [beerName, setBeerName] = useState<string>("");
+  const [abv, setAbv] = useState<string>("");
+  const [brewery, setBrewery] = useState<string>("");
+  const [country, setCountry] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  const { t } = useTranslation();
+
+  const handleSubmit = async () => {
+    setError("");
+    setLoading(true);
+
+    try {
+    } catch (err) {
+      setError("Adding beer failed");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  //internetin syövereistä, melkone mankeli
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images, //deprecated piece of s**t but works
+      quality: 0.7,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const takePhoto = async () => {
+    const permission = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (!permission.granted) {
+      setError("Camera permission required");
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+      quality: 0.7,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.scrollContent}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.BeerContent}>
+        <Text style={styles.title}>{t("addBeer.title")}</Text>
+        <View style={styles.imageBox}>
+          {image ? (
+            <>
+              <Image source={{ uri: image }} style={styles.imagePreview} />
+
+              <Pressable
+                style={styles.removeButton}
+                onPress={() => setImage(null)}
+              >
+                <Text style={styles.removeButtonText}>
+                  {t("addBeer.camera.removephoto")}
+                </Text>
+              </Pressable>
+            </>
+          ) : (
+            <>
+              <Ionicons name="add-circle-outline" size={64} color="#000000" />
+            </>
+          )}
+
+          <View style={styles.imageButtons}>
+            <Pressable style={styles.smallButton} onPress={takePhoto}>
+              <Text style={styles.smallButtonText}>
+                {t("addBeer.camera.usecamera")}
+              </Text>
+            </Pressable>
+
+            <Pressable style={styles.smallButton} onPress={pickImage}>
+              <Text style={styles.smallButtonText}>
+                {t("addBeer.camera.choosefile")}
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+        <Text style={styles.label}>{t("addBeer.name")}</Text>
+        <View style={styles.field}>
+          <TextInput
+            style={styles.input}
+            placeholder={t("addBeer.name")}
+            value={beerName}
+            onChangeText={setBeerName}
+          />
+          {
+            <Pressable
+              style={styles.clearButton}
+              onPress={() => setBeerName("")}
+            >
+              <Ionicons name="close-circle" size={27} color="#888" />
+            </Pressable>
+          }
+        </View>
+        <Text style={styles.label}>ABV</Text>
+        <View style={styles.field}>
+          <TextInput
+            style={styles.input}
+            placeholder="ABV (0.0%)"
+            value={abv}
+            onChangeText={setAbv}
+            keyboardType="decimal-pad"
+          />
+          {
+            <Pressable style={styles.clearButton} onPress={() => setAbv("")}>
+              <Ionicons name="close-circle" size={27} color="#888" />
+            </Pressable>
+          }
+        </View>
+        <Text style={styles.label}>{t("addBeer.brewery")}</Text>
+        <View style={styles.field}>
+          <TextInput
+            style={styles.input}
+            placeholder={t("addBeer.brewery")}
+            value={brewery}
+            onChangeText={setBrewery}
+          />
+          {
+            <Pressable
+              style={styles.clearButton}
+              onPress={() => setBrewery("")}
+            >
+              <Ionicons name="close-circle" size={27} color="#888" />
+            </Pressable>
+          }
+        </View>
+        <Text style={styles.label}>{t("addBeer.country")}</Text>
+        <View style={styles.field}>
+          <TextInput
+            style={styles.input}
+            placeholder={t("addBeer.country")}
+            value={country}
+            onChangeText={setCountry}
+          />
+          <Pressable style={styles.clearButton} onPress={() => setCountry("")}>
+            <Ionicons name="close-circle" size={27} color="#888" />
+          </Pressable>
+        </View>
+
+        {!!error && <Text style={styles.error}>{error}</Text>}
+
+        <View style={styles.buttonRow}>
+          <Pressable
+            style={[styles.button, loading && styles.buttonDisabled]}
+            onPress={handleSubmit}
+            disabled={loading}
+          >
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Text style={styles.buttonText}>{t("addBeer.addtodb")}</Text>
+            )}
+          </Pressable>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#888",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  BeerContent: {
+    width: "105%",
+    maxWidth: 360,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    padding: 16,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  field: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+  },
+  label: {
+    marginBottom: 6,
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+  },
+  button: {
+    marginTop: 8,
+    backgroundColor: "#6750a4",
+    borderRadius: 8,
+    paddingVertical: 12,
+    alignItems: "center",
+    width: "37%",
+  },
+  buttonDisabled: {
+    opacity: 0.7,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "700",
+  },
+  error: {
+    color: "#d00",
+    marginTop: 4,
+    marginBottom: 6,
+  },
+  imageBox: {
+    width: "100%",
+    height: 300,
+    borderWidth: 2,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    marginBottom: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+
+  imagePreview: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+
+  imagePlaceholder: {
+    color: "#888",
+    marginBottom: 10,
+  },
+
+  imageButtons: {
+    flexDirection: "column",
+    gap: 10,
+    width: "50%",
+  },
+
+  smallButton: {
+    backgroundColor: "#6750a4",
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  smallButtonText: {
+    color: "#fff",
+    fontWeight: "600",
+  },
+
+  buttonRow: {
+    marginTop: 12,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  removeButton: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    backgroundColor: "rgba(0,0,0,0.6)",
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    borderRadius: 6,
+  },
+
+  removeButtonText: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: "600",
+  },
+  clearButton: {
+    marginLeft: 8,
+  },
+});
