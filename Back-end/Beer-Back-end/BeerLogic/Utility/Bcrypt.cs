18e9e14dbdfabb33fac62a16580867fe9357aa63
qb-le <quocbaole2103@gmail.com>
2026-03-31 14:22:42 +0200

    Added the Bcrypt hasher

---
+++ Back-end/Beer-Back-end/BeerLogic/Utility/Bcrypt.cs
@@ -0,0 +1,24 @@
+﻿using BeerLogic.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace BeerLogic.Utility
+{
+    public class Bcrypt : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string enteredPassword, string storedHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(enteredPassword, storedHash);
+        }
+    }
+}
+
