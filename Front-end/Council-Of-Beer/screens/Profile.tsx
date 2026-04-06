e15ee86ea7a4328570c9a785d2d5661d51bf98f0
nellaSoudunsaari <nellasoudunsaari@gmail.com>
2026-04-06 14:09:59 +0300

    added modal for new collection

--- Front-end/Council-Of-Beer/screens/Profile.tsx
+++ Front-end/Council-Of-Beer/screens/Profile.tsx
@@ -1,13 +1,18 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View, ScrollView, Modal, TextInput } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 function CollectionObj() {
   const navigation = useNavigation();
   return(
-    <TouchableOpacity onPress={() => navigation.navigate('Collection')}>
-      <Image style={styles.collectionImg}/>
-    </TouchableOpacity>
+    
+      <TouchableOpacity onPress={() => navigation.navigate('Collection')}>
+        <View style={styles.singleCollection}>
+          <Image style={styles.collectionImg}/>
+          <Text style={styles.collectionTxt}>Beer collection</Text>
+        </View>
+      </TouchableOpacity>
   );
 }
 
@@ -21,6 +26,8 @@ function PostObj() {
 }
 
 export default function Profile(){
+  const navigation = useNavigation();
+  const [ modalVisible, setModalVisible ] = useState(false);
 
     return(
         <>
@@ -51,16 +58,52 @@ export default function Profile(){
               </View>
             </View>
           </View>
-          <ScrollView horizontal={ true } style={{height: 0, backgroundColor: '#1D190E'}}>
-              <CollectionObj/>
-              <CollectionObj/>
-              <CollectionObj/>
-              <CollectionObj/>
-              <CollectionObj/>
-              <CollectionObj/>
-              <CollectionObj/>
-              <CollectionObj/>
-          </ScrollView>
+          <View style={styles.collectionCont}>
+            <ScrollView horizontal={ true } style={{backgroundColor: '#1D190E'}}>
+                <TouchableOpacity onPress={() => setModalVisible(true)}>
+                  <Modal 
+                    backdropColor={'#00000025'}
+                    visible={modalVisible}
+                    onRequestClose={() => {setModalVisible(!modalVisible)}}
+                  >
+                    <View style={styles.modalCont}>
+                      <View style={styles.modalView}>
+                        <Text style={styles.modalTitle}>Create a new collection</Text>
+                        <View style={styles.newCollectionInfoView}>
+                          <Text style={styles.modalTxt}>New collection's name</Text>
+                          <TextInput
+                            style={styles.collectionNameField}
+                            placeholder='Enter a name...'
+                            placeholderTextColor={'#EDE9C7'}
+                          />
+                        </View>
+                        <View style={styles.modalBtnView}>
+                          <TouchableOpacity onPress={() => {setModalVisible(!setModalVisible)}} style={styles.modalBtn}>
+                            <Text>Close</Text>
+                          </TouchableOpacity>
+                          <TouchableOpacity onPress={() => {setModalVisible(!setModalVisible)}} style={styles.modalBtn}>
+                            <Text>Add</Text>
+                          </TouchableOpacity>
+                        </View>
+                      </View>
+                    </View>
+                  </Modal>
+                  <View style={styles.singleCollection}>
+                    <Ionicons name="add" size={32} color="#28200C" style={{position: 'absolute', top: 20, left: 30, zIndex: 9999}}/>
+                    <Image style={styles.collectionImg}/>
+                    <Text style={styles.collectionTxt}>Add new collection</Text>
+                  </View>
+                </TouchableOpacity>
+                <CollectionObj/>
+                <CollectionObj/>
+                <CollectionObj/>
+                <CollectionObj/>
+                <CollectionObj/>
+                <CollectionObj/>
+                <CollectionObj/>
+                <CollectionObj/>
+            </ScrollView>
+          </View>
           <View style={styles.profilecontent}>            
             <ScrollView style={{height: 0}}>
               <View style={styles.feed}>
@@ -87,7 +130,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     paddingTop: 20,
-    marginBottom: -50,
+    marginBottom: -70,
     backgroundColor: '#1D190E'
   },
   profilecontent: {
@@ -142,6 +185,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#EDE9C7'
   },
+  collectionCont: {
+    flexDirection: 'row',
+  },
+  singleCollection: {
+    alignItems: 'center'
+  },
   collectionImg: {
     width: 75,
     height: 75,
@@ -150,6 +199,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#EDE9C7',
     marginLeft: 10
+  },
+  collectionTxt: {
+    color: '#EDE9C7', 
+    fontSize: 14, 
+    maxWidth: 75, 
+    textAlign: 'center',
+    paddingLeft: 5
   },
   feedImg: {
     height: 150,
@@ -161,6 +217,53 @@ const styles = StyleSheet.create({
     flex: 0.33,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    width: '101%'
-  }
+    width: '101%',
+    paddingTop: 10
+  },
+  modalCont: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: '#28200C',
+    padding: 25,
+    borderRadius: 12
+  },
+  modalTitle: {
+    color: '#EDE9C7',
+    fontSize: 24,
+    fontFamily: "GermaniaOne400_Regular",
+  },
+  modalBtnView: {
+    flexDirection: 'row',
+    gap: 150
+  },
+  modalBtn: {
+    alignItems: 'center',
+    borderRadius: 4,
+    backgroundColor: '#E39914',
+    padding: 8,
+    width: 72
+  },
+  modalTxt: {
+    color: '#EDE9C7',
+  },
+  newCollectionInfoView: {
+    paddingLeft: 10,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  collectionNameField: {
+    backgroundColor: '#28200C',
+    color: '#EDE9C7',
+    padding: 10,
+    marginTop: 10,
+    marginLeft: 20,
+    fontSize: 16,
+    height: 40,
+    borderWidth: 0.5,
+    borderColor: '#EDE9C7',
+    borderRadius: 4,
+  },
 });
