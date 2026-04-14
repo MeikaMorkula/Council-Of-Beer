afcb9fe1b613ddac50a45866d2bbaa8ed248a653
WojtekZb <554101@student.fontys.nl>
2026-04-15 01:42:39 +0200

    Get all users, Follow and get Follow requests is working

---
+++ Back-end/Beer-Back-end/BeerLogic/Interface/ISocialRepo.cs
@@ -0,0 +1,11 @@
+﻿using BeerLogic.Entities;
+
+namespace BeerLogic.Interface
+{
+    public interface ISocialRepo
+    {
+        List<string> GetAllUsernames();
+        string FollowUser(string username, string followingUsername);
+        List<FollowRequestResponse> GetFollowRequests(string username);
+    }
+}
---
+++ Back-end/Beer-Back-end/BeerLogic/Service/SocialService.cs
@@ -0,0 +1,30 @@
+﻿using BeerLogic.Entities;
+using BeerLogic.Interface;
+
+namespace BeerLogic.Service
+{
+    public class SocialService
+    {
+        private readonly ISocialRepo _socialRepo;
+
+        public SocialService(ISocialRepo socialRepo)
+        {
+            _socialRepo = socialRepo;
+        }
+
+        public List<string> GetAllUsernames()
+        {
+            return _socialRepo.GetAllUsernames();
+        }
+
+        public string FollowUser(string username, string followingUsername)
+        {
+            return _socialRepo.FollowUser(username, followingUsername);
+        }
+
+        public List<FollowRequestResponse> GetFollowRequests(string username)
+        {
+            return _socialRepo.GetFollowRequests(username);
+        }
+    }
+}
