ded715a655b8328bf0f5ff9b1fb8f91953726dee
WojtekZb <554101@student.fontys.nl>
2026-04-21 13:35:35 +0200

    Fixed post

--- Back-end/Beer-Back-end/BeerData/Repository/ImageHandlerRepo.cs
+++ Back-end/Beer-Back-end/BeerData/Repository/ImageHandlerRepo.cs
@@ -27,12 +27,12 @@ namespace BeerData.Repository
 
                 string query = @"
                     WITH inserted_post AS (
-                        INSERT INTO post (userid, beerid, description, bar, city, createdat, image_url, image_public_id)
-                        SELECT u.id, b.id, @description, @bar, @city, @createdat, @image_url, @image_public_id
+                        INSERT INTO post (user_id, beer_id, description, bar, city, image_url, image_public_id)
+                        SELECT u.id, b.id, @description, @bar, @city, @image_url, @image_public_id
                         FROM users u
                         JOIN beer b ON b.name = @beerName
                         WHERE u.name = @userName
-                        RETURNING id, userid, beerid, description, bar, city, image_url, image_public_id
+                        RETURNING id, user_id, beer_id, description, bar, city, image_url, image_public_id
                     )
                     SELECT 
                         u.name AS username,
@@ -41,18 +41,16 @@ namespace BeerData.Repository
                         p.description,
                         p.bar,
                         p.city,
-                        p.createdat,
                         p.image_url
                     FROM inserted_post p
-                    JOIN users u ON u.id = p.userid
-                    JOIN beer b ON b.id = p.beerid;
+                    JOIN users u ON u.id = p.user_id
+                    JOIN beer b ON b.id = p.beer_id;
                 ";
 
                 using var command = new NpgsqlCommand(query, connection);
                 command.Parameters.AddWithValue("@userName", post.username);
                 command.Parameters.AddWithValue("@beerName", post.beername);
                 command.Parameters.AddWithValue("@description", (object?)post.Description ?? DBNull.Value);
-                command.Parameters.AddWithValue("@createdat", DateTime.UtcNow);
                 command.Parameters.AddWithValue("@bar", (object?)post.Bar ?? DBNull.Value);
                 command.Parameters.AddWithValue("@city", (object?)post.City ?? DBNull.Value);
                 command.Parameters.AddWithValue("@image_url", (object?)uploadResult.ImageUrl ?? DBNull.Value);
